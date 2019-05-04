448231e6e6e5c3a754b1a4b00bf824bc938be969
Vikram Kulkarni <vikram@vikcon.in>
2019-05-04 09:58:15 +0530

    WooCommerce Code Snippet uploaded

---
+++ payment gateway selector.php
@@ -0,0 +1,19 @@
+<?php   
+function vikcon_payment_gateway_disable_country( $available_gateways ) {
+    if ( is_admin() ) return $available_gateways;
+    
+    /* Disable Paypal in India */
+    if ( isset( $available_gateways['paypal'] ) && WC()->customer->get_billing_country() == 'IN' ) {
+            unset( $available_gateways['paypal'] );
+    }
+
+    /* Disable Indian gateway outside India */
+    if ( isset( $available_gateways['instamojo'] ) && WC()->customer->get_billing_country() <> 'IN' ) {
+            unset( $available_gateways['instamojo'] );
+    }
+    
+    return $available_gateways;
+}
+
+add_filter( 'woocommerce_available_payment_gateways', 'wpmudra_payment_gateway_disable_country' );
+?>
